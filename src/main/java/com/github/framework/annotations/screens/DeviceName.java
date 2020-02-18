25fa20c73b10c61421033f5028f8a550860840a9
Ray Zhou <ray.zhou.zhj@gmail.com>
2020-02-18 16:44:34 +0800

    Update DeskTopHD resolution to 1680x1050

--- src/main/java/com/github/framework/annotations/screens/DeviceName.java
+++ src/main/java/com/github/framework/annotations/screens/DeviceName.java
@@ -7,7 +7,7 @@ public enum DeviceName {
 	GalaxyS5(360, 640),
 	Pixel2XL(411, 823),
 	OtherDevice(0,0),
-	DeskTopHD(1366, 768);
+	DeskTopHD(1680, 1050);
 	
 	public int width = 0;
 	public int height = 0;

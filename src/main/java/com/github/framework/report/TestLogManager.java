0cdf7b7af1127b555c2a2e540f92cfcc198d8671
RayZhou <ray.zhou@scmp.com>
2020-02-20 11:40:17 +0800

    Fixed relative path to failure screenshot

--- src/main/java/com/github/framework/report/TestLogManager.java
+++ src/main/java/com/github/framework/report/TestLogManager.java
@@ -1,6 +1,8 @@
 package com.github.framework.report;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 import org.testng.ITestResult;
 
@@ -11,97 +13,85 @@ import com.github.framework.utils.ScreenShotManager;
 /**
  *
  */
-public class TestLogManager
-{
-	private ScreenShotManager screenShotManager;
+public class TestLogManager {
+    private ScreenShotManager screenShotManager;
 
-	public TestLogManager()
-	{
-		screenShotManager = new ScreenShotManager();
-	}
+    public TestLogManager() {
+        screenShotManager = new ScreenShotManager();
+    }
 
 
-	public void endLog(ITestResult result, ThreadLocal<ExtentTest> test) throws IOException, InterruptedException 
-	{
-		String className = result.getInstance().getClass().getSimpleName();
-		if(result.isSuccess())
-		{
-			test.get().log(Status.PASS, "Test Passed: " + result.getMethod().getMethodName());
-		}
-		else
-		{
-			if (result.getStatus() == ITestResult.FAILURE) 
-			{
-				/*
-				 * Failure Block
-				 */
-				handleTestFailure(result, className, test);
-			}
-		}
+    public void endLog(ITestResult result, ThreadLocal<ExtentTest> test) throws IOException, InterruptedException {
+        String className = result.getInstance().getClass().getSimpleName();
+        if (result.isSuccess()) {
+            test.get().log(Status.PASS, "Test Passed: " + result.getMethod().getMethodName());
+        } else {
+            if (result.getStatus() == ITestResult.FAILURE) {
+                /*
+                 * Failure Block
+                 */
+                handleTestFailure(result, className, test);
+            }
+        }
 
-		/*
-		 * Skip block
-		 */
-		if (result.getStatus() == ITestResult.SKIP) 
-		{
-			test.get().log(Status.SKIP, "Test skipped");
-		}
+        /*
+         * Skip block
+         */
+        if (result.getStatus() == ITestResult.SKIP) {
+            test.get().log(Status.SKIP, "Test skipped");
+        }
 
-		ExtentManager.getExtent().flush();
-	}
+        ExtentManager.getExtent().flush();
+    }
 
-	public String getClassName(String s) 
-	{
-		final String classNameCur = s.substring(1);
-		final Package[] packages = Package.getPackages();
-		String className = null;
+    public String getClassName(String s) {
+        final String classNameCur = s.substring(1);
+        final Package[] packages = Package.getPackages();
+        String className = null;
 
-		for (final Package p : packages) 
-		{
-			final String pack = p.getName();
-			final String tentative = pack + "." + classNameCur;
-			try 
-			{
-				Class.forName(tentative);
-			} 
-			catch (final ClassNotFoundException e) 
-			{
-				continue;
-			}
+        for (final Package p : packages) {
+            final String pack = p.getName();
+            final String tentative = pack + "." + classNameCur;
+            try {
+                Class.forName(tentative);
+            } catch (final ClassNotFoundException e) {
+                continue;
+            }
 
-			className = tentative;
-			break;
-		}
+            className = tentative;
+            break;
+        }
 
-		return className;
-	}
+        return className;
+    }
 
-	private void handleTestFailure(ITestResult result, String className, ThreadLocal<ExtentTest> test)
-	{
-		if (result.getStatus() == ITestResult.FAILURE) 
-		{
-			// Print exception stack trace if any
-			Throwable throwable = result.getThrowable();
-			if(throwable != null)
-			{
-				throwable.printStackTrace();
-				test.get().log(Status.FAIL, "<pre>" + result.getThrowable().getMessage() + "</pre>");
-			}
+    private void handleTestFailure(ITestResult result, String className, ThreadLocal<ExtentTest> test) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            // Print exception stack trace if any
+            Throwable throwable = result.getThrowable();
+            if (throwable != null) {
+                throwable.printStackTrace();
+                test.get().log(Status.FAIL, "<pre>" + result.getThrowable().getMessage() + "</pre>");
+            }
 
-			try
-			{
-				String screenShotPath = screenShotManager.captureScreenShot(
-						Status.FAIL,
-						result.getInstance().getClass().getSimpleName(),
-						result.getMethod().getMethodName());
+            try {
+                String screenShotAbsolutePath = screenShotManager.captureScreenShot(
+                        Status.FAIL,
+                        result.getInstance().getClass().getSimpleName(),
+                        result.getMethod().getMethodName());
 
-				System.out.println("Screenshot: " + screenShotPath);
-				test.get().addScreenCaptureFromPath(screenShotPath);
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-	}
+                String screenShotRelativePath = getRelativePathToReport(screenShotAbsolutePath);
+
+                test.get().addScreenCaptureFromPath(screenShotRelativePath);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getRelativePathToReport(String file) {
+        Path path = new File(file).toPath();
+        Path targetPath = new File(System.getProperty("user.dir") + File.separator + "target").toPath();
+        return targetPath.relativize(path).toString();
+    }
 }
